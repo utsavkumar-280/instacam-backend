fad800643b2f8d8044877ee24aeb2df5ef3f5af4
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2021-07-20 09:32:27 +0530

    chore: initialized some files

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config();
+
+const dbConnection = require("./db/dbConnect.js");
+// const userAuthorization = require("./middlewares/userAuthorization");
+const route404Handler = require("./middlewares/route404Handler");
+const errorHandler = require("./middlewares/errorHandler");
+
+const app = express();
+const PORT = process.env.PORT || 8040;
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
+app.use(cors());
+
+dbConnection();
+
+app.get("/", (req, res) => {
+	res.send("Welcome to InstaCam Apis");
+});
+
+app.get("/hello", (req, res) => {
+	res.json({
+		success: true,
+		response: "Kya bolti Public!!",
+	});
+});
+
+//DO NOT MOVE THESE HANDLERS
+// 404 Route Handler
+app.use(route404Handler);
+
+//Error Handeler
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+	console.log(`server running on http://localhost:${PORT}`);
+});
