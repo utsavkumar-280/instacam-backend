ee4b7112583a56544bec5d6946e3f51cb4c79513
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-02-10 09:30:01 +0530

    refact: added new logic to timeFormatter

--- controllers/utils/timeFormatter.js
+++ controllers/utils/timeFormatter.js
@@ -2,22 +2,33 @@ const timeFormatter = (time) => {
 	const totalMin = Math.floor((Date.now() - time.getTime()) / (1000 * 60));
 
 	if (totalMin <= 0) {
-		return "Just now";
+		const totalSec = Math.floor((Date.now() - time.getTime()) / 1000);
+
+		if (totalSec <= 0) {
+			return "1s";
+		}
+		return `${totalSec}s`;
 	}
 	if (totalMin < 60) {
-		if (totalMin === 1) {
-			return `${totalMin} minute ago`;
-		}
-		return `${totalMin} minutes ago`;
+		return `${totalMin}min`;
 	}
 	if (totalMin >= 60 && totalMin < 1440) {
 		const totalHrs = Math.floor(totalMin / 60);
-		if (totalHrs === 1) {
-			return `${totalHrs} hour ago`;
-		}
-		return `${totalHrs} hours ago`;
+
+		return `${totalHrs}h`;
 	} else {
-		return time.toDateString();
+		var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+		const currentYear = new Date().getFullYear();
+		const postYear = time.getFullYear();
+		const postDay = days[time.getDay()];
+		let postFullDate = time.toDateString();
+		postFullDate = postFullDate.replace(`${postDay} `, "");
+
+		if (currentYear === postYear) {
+			postFullDate = postFullDate.replace(` ${postYear}`, "");
+			return postFullDate;
+		}
+		return postFullDate;
 	}
 };
 
